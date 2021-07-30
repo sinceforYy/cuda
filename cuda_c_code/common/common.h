da887806ebe9c816ba1315fac5358db653d78133
sinceforYy <1017657683@qq.com>
2021-07-30 11:35:30 +0800

    message

--- cuda_c_code/common/common.h
+++ cuda_c_code/common/common.h
@@ -14,4 +14,12 @@
 	}											\
 }
 
+inline double seconds()
+{
+	struct timeval tp;
+	struct timezone tzp;
+	int i = gettimeofday(&tp, &tzp);
+	return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+}
+
 #endif
